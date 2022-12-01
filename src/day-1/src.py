35d391797ed1496ae01b9d04ede88e1da77febc2
Raymond Gines <raymond.gines1@cfacorp.com>
2022-12-01 13:43:45 -0500

    add implementation for first gold star

--- src/day-1/src.py
+++ src/day-1/src.py
@@ -1,14 +1,19 @@
 import logging
 
-def read_elf_calories():
+
+def read_elf_calories(text_file_name):
+    logging.info("In: read_elf_calories()")
     lines = None
 
-    with open('calories.txt') as f:
+    with open(text_file_name) as f:
         lines = f.readlines()
 
+    logging.info("Out: read_elf_calories()")
     return lines
 
-def get_max_calories(calories):
+
+def get_elf_calories(calories):
+    logging.info("In: get_max_calories()")
     elf_calories_total = 0
     elf_calories = []
 
@@ -20,14 +25,21 @@ def get_max_calories(calories):
         else:
             elf_calories_total += int(i)
     
+    elf_calories.append(elf_calories_total)
+
+    logging.info("Out: get_max_calories()")
     return elf_calories
 
 
 if __name__ == '__main__':
-    print("Beginning")
     logging.info("Starting program")
 
-    lines = read_elf_calories()
-    max_calories = get_max_calories(lines)
-    
+    text_file_name = 'calories.txt'
+
+    lines = read_elf_calories(text_file_name)
+    elf_calories = get_elf_calories(lines)
+    max_calories = max(elf_calories)
+
+    print(max_calories)
+
     logging.info("Ending program")
