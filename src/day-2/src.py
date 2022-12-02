b1ce41d02db4d651deafdd410cfb14df9db1a2af
Raymond Gines <raymond.gines1@cfacorp.com>
2022-12-02 15:21:49 -0500

    implement day 2 part 2 problem

--- src/day-2/src.py
+++ src/day-2/src.py
@@ -20,15 +20,15 @@ if __name__ == "__main__":
     total = 0
 
     scoring_table = {
-        'A X': 4,
+        'A X': 3,
         'B X': 1,
-        'C X': 7,
-        'A Y': 8,
+        'C X': 2,
+        'A Y': 4,
         'B Y': 5,
-        'C Y': 2, 
-        'A Z': 3,
+        'C Y': 6, 
+        'A Z': 8,
         'B Z': 9,
-        'C Z': 6 
+        'C Z': 7 
     }
 
     for i in result:
