41a651269eadac3fd2fc38e33289716f633a902c
Raymond Gines <raymond.gines1@cfacorp.com>
2022-12-02 15:10:10 -0500

    implement day 2 part 1

--- src/day-2/src.py
+++ src/day-2/src.py
@@ -17,22 +17,9 @@ def read_input(text_file_name):
 if __name__ == "__main__":
     result = read_input('sample.txt')
 
-    player1_total = 0
-    player2_total = 0
+    total = 0
 
-    player1_scores = {
-        'A X': 4,
-        'B X': 8,
-        'C X': 3,
-        'A Y': 1,
-        'B Y': 5,
-        'C Y': 9, 
-        'A Z': 7,
-        'B Z': 2,
-        'C Z': 6 
-    }
-
-    player2_scores = {
+    scoring_table = {
         'A X': 4,
         'B X': 1,
         'C X': 7,
@@ -45,8 +32,6 @@ if __name__ == "__main__":
     }
 
     for i in result:
-        player1_total += player1_scores[i]
-        player2_total += player2_scores[i]
+        total += scoring_table[i]
         
-    print(f'Player 1 Score: { player1_total }')
-    print(f'Player 2 Score: { player1_total }')
+    print(f'Total Score: { total }')
